e4b78a6fd2a5c79a3ccf417286f9a99e65b69605
ManuelRafaelano <manuelrafaelano@gmail.com>
2019-06-10 17:00:21 -0600

    Update app.js

--- app.js
+++ app.js
@@ -5,5 +5,23 @@ const compuerScore_span = document.getElementById("computer-score");
 const scoreBoard_div = document.querySelector(".score-board");
 const result_div = document.querySelector(".result");
 const rock_div = document.getElementById("r");
-const paper = document.getElementById("p");
-const scissors = document.getElementById("s");
+const paper_div = document.getElementById("p");
+const scissors_div = document.getElementById("s");
+function getCompuerChoice() {
+  const choices = ["r", "p", "s"];
+  console.log(Math.random() * 3);
+}
+getCompuerChoice();
+function game(userChoice) {}
+function main() {
+  rock_div.addEventListener("click", function() {
+    game("r");
+  });
+  paper_div.addEventListener("click", function() {
+    game("p");
+  });
+  scissors_div.addEventListener("click", function() {
+    game("s");
+  });
+}
+main();
